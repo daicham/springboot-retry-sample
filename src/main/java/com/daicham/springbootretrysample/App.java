6bbf6e6aae60bd371306714cbbbfc7e137276017
daicham <daicham@gmail.com>
2017-07-01 22:40:06 +0900

    Add SpringBoot

---
+++ src/main/java/com/daicham/springbootretrysample/App.java
@@ -0,0 +1,15 @@
+package com.daicham.springbootretrysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author daicham
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String... args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
